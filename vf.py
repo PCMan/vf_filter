af6931c410ba8ae28f0d6165f828bf96192a6f05
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2016-03-16 16:13:35 +0800

    Perform resampling when reading the signals.

--- vf.py
+++ vf.py
@@ -19,7 +19,7 @@ def get_records(db_name):
     return records
 
 
-def load_record(db_name, record):
+def load_record(db_name, record, sample_rate=None):
     record_name = "{0}/{1}".format(db_name, record)
 
     # query number of channels in this record
@@ -30,11 +30,18 @@ def load_record(db_name, record):
     sample_buf = (wfdb.WFDB_Sample * n_channels)()
     signals = []
     if wfdb.isigopen(record_name, byref(sigInfo), n_channels) == n_channels:
+        # ask wfdb to perform re-sampling
+        if sample_rate:
+            # https://www.physionet.org/physiotools/wpg/wpg_20.htm#getvec
+            # If setifreq has been used to modify the input sampling rate, getvec resamples the input signals at the
+            # desired rate, using linear interpolation between the pair of samples nearest in time to that of the
+            # sample to be returned.
+            wfdb.setifreq(wfdb.WFDB_Frequency(sample_rate))
+
         while wfdb.getvec(byref(sample_buf)) > 0:
             sample = sample_buf[0]  # we only want the first channel
             signals.append(sample)
     signals = np.array(signals)
-    print "  # of samples:", len(signals)
 
     # read annotations
     annotations = []
@@ -53,7 +60,7 @@ def load_record(db_name, record):
                 # the first byte of aux is the length of the string
                 aux_ptr = cast(ann.aux, c_void_p).value + 1  # skip the first byte
                 rhythm_type = cast(aux_ptr, c_char_p).value
-            print ann.time, wfdb.anndesc(ann_code), wfdb.annstr(ann_code), rhythm_type
+            # print ann.time, wfdb.anndesc(ann_code), wfdb.annstr(ann_code), rhythm_type
             annotations.append((ann.time, ann_code, rhythm_type))
 
     return signals, annotations
@@ -64,6 +71,7 @@ if __name__ == "__main__":
     for db_name in ("mitdb", "vfdb", "cudb"):
         for record in get_records(db_name):
             print "READ SIGNAL:", db_name, record
-            signals, annotations = load_record(db_name, record)
+            signals, annotations = load_record(db_name, record, sample_rate=250.0)
+            print "  # of samples:", len(signals), ", # of anns:", len(annotations)
 
     wfdb.wfdbquit()
