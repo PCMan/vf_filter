9c9a9c4d70e84d7910aef67799f8b13df9e862aa
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2016-04-15 14:47:39 +0800

    Move evaluation related to code to its own module.

---
+++ vf_eval.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# import pyximport; pyximport.install()
+# evaluation tools for classification
+import numpy as np
+
+
+def balanced_error_rate(y_true, y_predict):
+    incorrect = (y_true != y_predict)
+    fp = np.sum(np.logical_and(y_predict, incorrect))
+    pred_negative = np.logical_not(y_predict)
+    fn = np.sum(np.logical_and(pred_negative, incorrect))
+    n_positive = np.sum(y_true)
+    n_negative = len(y_true) - n_positive
+    return 0.5 * (fn / n_positive + fp / n_negative)
+
+
+class ClassificationResult:
+    def __init__(self, y_true, y_predict):
+        correct = (y_true == y_predict)
+        incorrect = np.logical_not(correct)
+        pred_negative = np.logical_not(y_predict)
+        tp = np.sum(np.logical_and(y_predict, correct))
+        fp = np.sum(np.logical_and(y_predict, incorrect))
+        tn = np.sum(np.logical_and(pred_negative, correct))
+        fn = np.sum(np.logical_and(pred_negative, incorrect))
+        self.sensitivity = tp / (tp + fn)
+        self.specificity = tn / (tn + fp)
+        self.precision = tp / (tp + fp)
+        self.accuracy = (tp + tn) / len(y_true)
+        self.tp = tp
+        self.tn = tn
+        self.fp = fp
+        self.fn = fn
