c27e6c57028c869c7ffb91d2a993893f69632d96
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2016-04-16 22:46:29 +0800

    Add type info for numpy arrays to use cython to speed up indexing.

--- vf_features.pyx
+++ vf_features.pyx
@@ -3,7 +3,8 @@
 # This implements common VF related features reported in the literatures.
 # Felipe AA et al. 2014. Detection of Life-Threatening Arrhythmias Using Feature Selection and Support Vector Machines
 import pyximport; pyximport.install()  # use Cython
-import numpy as np
+# import numpy as np
+cimport numpy as np  # use cython to speed up numpy array access (http://docs.cython.org/src/userguide/numpy_tutorial.html)
 import scipy.signal as signal
 import pyeeg  # calculate sample entropy
 import matplotlib.pyplot as plt
@@ -14,7 +15,7 @@ feature_names = ("TCSC", "TCI", "STE", "MEA", "PSR", "HILB", "VF", "M", "A2", "F
 
 # time domain/morphology
 
-cdef threshold_crossing_count(samples, double threshold_ratio=0.2):
+cdef tuple threshold_crossing_count(np.ndarray[double, ndim=1] samples, double threshold_ratio=0.2):
     cdef double threshold = threshold_ratio * np.max(samples)
     cdef int n_cross = 0
     cdef bint higher = samples[0] >= threshold
@@ -48,7 +49,7 @@ cdef threshold_crossing_count(samples, double threshold_ratio=0.2):
 # TCSC feature
 # Muhammad Abdullah Arafat et al. 2011. A simple time domain algorithm for the detection of
 # ventricular fibrillation in electrocardiogram.
-cdef double threshold_crossing_sample_counts(samples, int sampling_rate, double window_duration=3.0, double threshold_ratio=0.2):
+cdef double threshold_crossing_sample_counts(np.ndarray[double, ndim=1] samples, int sampling_rate, double window_duration=3.0, double threshold_ratio=0.2):
     # steps:
     # 1. multiply the samples by a cosine window
     #  w(t) = 1/2(1-cos(4*pi*t)), if 0 <= t <= 1/4 or Ls-1/4 <= t <= Ls
@@ -67,6 +68,7 @@ cdef double threshold_crossing_sample_counts(samples, int sampling_rate, double 
 
     tcsc = array("d")
     cdef double n = 0
+    cdef np.ndarray[double, ndim=1] window
     while window_end <= n_samples:
         # moving window
         window = samples[window_begin:window_end]
@@ -90,7 +92,7 @@ cdef double threshold_crossing_sample_counts(samples, int sampling_rate, double 
 
 
 # average threshold crossing interval
-cdef double threshold_crossing_intervals(samples, int sampling_rate, double threshold_ratio=0.2):
+cdef double threshold_crossing_intervals(np.ndarray[double, ndim=1] samples, int sampling_rate, double threshold_ratio=0.2):
     cdef int n_samples = len(samples)
     cdef int window_size = int(sampling_rate)  # calculate 1 TCI value per second
     cdef int window_begin = 0
@@ -100,6 +102,7 @@ cdef double threshold_crossing_intervals(samples, int sampling_rate, double thre
 
     cdef list results = []
     cdef int n_cross, first_cross, last_cross
+    cdef np.ndarray[double, ndim=1] window
     while window_end <= n_samples:
         window = samples[window_begin:window_end]
         n_cross, first_cross, last_cross = threshold_crossing_count(window, threshold_ratio)
@@ -164,7 +167,7 @@ cdef double standard_exponential(samples, int sampling_rate, int time_constant=3
     return n_crosses / duration
 
 
-cdef double modified_exponential(samples, int sampling_rate, double time_constant=0.2):
+cdef double modified_exponential(np.ndarray[double, ndim=1] samples, int sampling_rate, double time_constant=0.2):
     # similar to standard exponential, but uses relative max, not global max.
     # lift the exponential curve to the relative max again at every intersection.
     # E(t) = Mj * exp(-(t - tm,j) / T), tm,j <= t <= tc,j
@@ -209,7 +212,7 @@ cdef double modified_exponential(samples, int sampling_rate, double time_constan
 # Emran M Abu Anas et al. 2010. Sequential algorithm for life threatening cardiac pathologies detection based on
 # mean signal strength and EMD functions.
 # http://biomedical-engineering-online.biomedcentral.com/articles/10.1186/1475-925X-9-43
-cdef double mean_absolute_value(samples, int sampling_rate, double window_duration=2.0):
+cdef double mean_absolute_value(np.ndarray[double, ndim=1] samples, int sampling_rate, double window_duration=2.0):
     # pre-processing: mean subtraction
     # FIXME: the samples we got here already received normalization, which is different from that in the original paper
     cdef int n_samples = len(samples)
@@ -218,6 +221,7 @@ cdef double mean_absolute_value(samples, int sampling_rate, double window_durati
     cdef int window_begin = 0
     cdef int window_end = window_size
     # 2-sec moving window with 1 sec step
+    cdef np.ndarray[double, ndim=1] window_samples
     while window_end <= n_samples:
         # normalization within the window
         window_samples = samples[window_begin:window_end]
@@ -233,7 +237,7 @@ cdef double mean_absolute_value(samples, int sampling_rate, double window_durati
 # 2007. Anton Amann et al. Detecting Ventricular Fibrillation by Time-Delay Methods
 # Plotting the time sequence on a phase space plot, and then calculate the boxes
 # visited in a 40x40 grid.
-cdef double phase_space_reconstruction(samples, int sampling_rate, double delay=0.5):
+cdef double phase_space_reconstruction(np.ndarray[double, ndim=1] samples, int sampling_rate, double delay=0.5):
     # phase space plotting
     # each data point is: x: x(t), y: x(t + T), where T = 0.5 s by default.
     cdef int n_samples = len(samples)
@@ -245,10 +249,10 @@ cdef double phase_space_reconstruction(samples, int sampling_rate, double delay=
     cdef double axis_range = np.max(samples) - offset
 
     # convert X and Y values to indices of the 40 x 40 grid
-    grid_x = ((x_samples - offset) * 39.0 / axis_range).astype("int")
-    grid_y = ((y_samples - offset) * 39.0 / axis_range).astype("int")
+    cdef np.ndarray[int, ndim=1] grid_x = ((x_samples - offset) * 39.0 / axis_range).astype("int")
+    cdef np.ndarray[int, ndim=1] grid_y = ((y_samples - offset) * 39.0 / axis_range).astype("int")
 
-    grid = np.zeros((40, 40))
+    cdef np.ndarray[int, ndim=1] grid = np.zeros((40, 40), dtype="int")
     grid[grid_y, grid_x] = 1
     return np.sum(grid) / 1600
 
@@ -256,7 +260,7 @@ cdef double phase_space_reconstruction(samples, int sampling_rate, double delay=
 # HILB feature based on Hilbert transformation + phase space plot
 # A Amann et al. 2005. A New Ventricular Fibrillation Detection Algorithm for Automated External Defibrillators
 # Computers in Cardiology 2005;32:559−562.
-cdef double hilbert_psr(samples, int sampling_rate):
+cdef double hilbert_psr(np.ndarray[double, ndim=1] samples, int sampling_rate):
     # each data point is: x: x(t), y: xH(t), where xH(t) is the Hilbert transform of x(t)
     cdef float duration = len(samples) / sampling_rate
     # down sample to 50 Hz for fast computation
@@ -273,36 +277,28 @@ cdef double hilbert_psr(samples, int sampling_rate):
     cdef double y_range = np.max(y_samples) - y_offset
 
     # convert X and Y values to indices of the 40 x 40 grid
-    grid_x = ((x_samples - x_offset) * 39.0 / x_range).astype("int")
-    grid_y = ((y_samples - y_offset) * 39.0 / y_range).astype("int")
+    cdef np.ndarray[int, ndim=1] grid_x = ((x_samples - x_offset) * 39.0 / x_range).astype("int")
+    cdef np.ndarray[int, ndim=1] grid_y = ((y_samples - y_offset) * 39.0 / y_range).astype("int")
 
     # calculate the number of visited cells
-    grid = np.zeros((40, 40))
+    cdef np.ndarray[int, ndim=1] grid = np.zeros((40, 40))
     grid[grid_y, grid_x] = 1
     return np.sum(grid) / 1600
 
 
 # Bandpass filter:
 # http://scipy.github.io/old-wiki/pages/Cookbook/ButterworthBandpass
-cdef butter_bandpass_filter(data, double lowcut, double highcut, double fs, int order=5):
+cdef np.ndarray[double, ndim=1] butter_bandpass_filter(np.ndarray[double, ndim=1] data, double lowcut, double highcut, double fs, int order=5):
     cdef double nyq = 0.5 * fs
     cdef double low = lowcut / nyq
     cdef double high = highcut / nyq
+    cdef np.ndarray[double, ndim = 1] b, a
     b, a = signal.butter(order, [low, high], btype='band')
-    y = signal.lfilter(b, a, data)
+    cdef np.ndarray[double, ndim=1] y = signal.lfilter(b, a, data)
     return y
 
 
-'''
-cdef butter_lowpass_filter(data, double highcut, double fs, int order=5):
-    cdef double nyq = 0.5 * fs
-    cdef double high = highcut / nyq
-    b, a = signal.butter(order, high, btype='low')
-    y = signal.lfilter(b, a, data)
-    return y
-'''
-
-cdef moving_average(samples, int order=5):
+cdef np.ndarray[double, ndim=1] moving_average(np.ndarray[double, ndim=1] samples, int order=5):
     # Reference: https://www.otexts.org/fpp/6/2
     # Moving average can be calculated using convolution
     # http://matlabtricks.com/post-11/moving-average-by-convolution
@@ -310,7 +306,7 @@ cdef moving_average(samples, int order=5):
 
 
 # the VF leak algorithm
-cdef double vf_leak(samples, fft, fft_freq):
+cdef double vf_leak(np.ndarray[double, ndim=1] samples, np.ndarray[double, ndim=1] fft, np.ndarray[double, ndim=1] fft_freq):
     # This method separates nearly sinusoidal waveforms from the rest.
     # VF is nearly sinusoidal. The idea is to move such signal by half period
     # trying to minimize the sum of the signal and its shifted copy.
@@ -328,20 +324,20 @@ cdef double vf_leak(samples, fft, fft_freq):
         cycle = len(samples)
     cdef double half_cycle = int(cycle/2)
 
-    original = samples[half_cycle:]
-    shifted = samples[:-half_cycle]
+    cdef np.ndarray[double, ndim=1] original = samples[half_cycle:]
+    cdef np.ndarray[double, ndim=1] shifted = samples[:-half_cycle]
     return np.sum(np.abs(original + shifted)) / np.sum(np.abs(original) + np.abs(shifted))
 
 
 # spectral parameters (M and A2)
-cdef tuple spectral_features(fft, fft_freq, int sampling_rate):
+cdef tuple spectral_features(np.ndarray[double, ndim=1] fft, np.ndarray[double, ndim=1] fft_freq, int sampling_rate):
     # Find the peak frequency within the range of 0.5 Hz - 9.0 Hz
     # NOTE: the unit of time is sample number, so the unit of frequency is not Hz here
     # to convert to Hz, we have to multiply the frequencies with sample rate.
     # fft_freq_hz = fft_freq * sampling_rate
 
     # The original paper approximate the amplitude by real + imaginary parts instead of true modulus.
-    amplitudes = np.abs(fft)
+    cdef np.ndarray[double, ndim=1] amplitudes = np.abs(fft)
 
     cdef int min_freq_idx = np.searchsorted(fft_freq, 0.5 / sampling_rate, side="right")
     cdef int max_freq_idx = np.searchsorted(fft_freq, 9.0 / sampling_rate, side="left")
@@ -360,7 +356,7 @@ cdef tuple spectral_features(fft, fft_freq, int sampling_rate):
     cdef double spectral_moment = 0.0
     cdef double spec_max_freq = min(20 * peak_freq, 100.0 / sampling_rate)  # convert 100 Hz to use sample count as time unit
     cdef int max_spec_idx = np.searchsorted(fft_freq, spec_max_freq, side="left")
-    m_amplitudes = amplitudes[0:max_spec_idx]
+    cdef np.ndarray[double, ndim=1] m_amplitudes = amplitudes[0:max_spec_idx]
     cdef double sum_m_amplitudes = np.sum(m_amplitudes)
 
     if sum_m_amplitudes != 0:  # in theory, division by zero should not happen here
@@ -381,10 +377,10 @@ cdef tuple spectral_features(fft, fft_freq, int sampling_rate):
 # FM feature: central frequency that biset the power spectrum
 # IEEE TRANSACTIONS ON BIOMEDICAL ENGINEERING, VOL 37, NO 6. JUNE 1990
 # The Median Frequency of the ECG During Ventricular Fibrillation: Its Use in an Algorithm for Estimating the Duration of Cardiac Arrest.
-cdef double central_frequency(fft, fft_freq, int sampling_rate):
+cdef double central_frequency(np.ndarray[double, ndim=1] fft, np.ndarray[double, ndim=1] fft_freq, int sampling_rate):
     # FM = sum(fi * pi) / sum(pi)
     #   pi: power component at ith frequency
-    power_spec = np.abs(fft) ** 2
+    cdef np.ndarray[double, ndim = 1] power_spec = np.abs(fft) ** 2
     cdef double central_freq = np.dot(fft_freq, power_spec) / np.sum(power_spec)
     # convert the frequency to Hz
     return central_freq * sampling_rate
@@ -394,11 +390,11 @@ cdef double central_frequency(fft, fft_freq, int sampling_rate):
 # The implementation provided by sampen python package is also slow.
 # here we use sample entropy implemented by PyEEG project, which is a little bit faster.
 # https://github.com/forrestbao/pyeeg
-cdef double sample_entropy(samples):
+cdef double sample_entropy(np.ndarray[double, ndim=1] samples):
     # Ref: Haiyan Li. 2009 Detecting Ventricular Fibrillation by Fast Algorithm of Dynamic Sample Entropy
     # N = 1250 , r = 0.2 x SD, m = 2 worked well for the characterization ECG signals.
     # N = 1250 = 250 Hz x 5 seconds (5-sec window)
-    spen_samples = samples[-1250:]
+    cdef np.ndarray[double, ndim = 1] spen_samples = samples[-1250:]
     cdef double sample_sd = np.std(spen_samples)
     cdef double spen = pyeeg.samp_entropy(spen_samples, M=2, R=(0.2 * sample_sd))
     return spen
@@ -406,7 +402,7 @@ cdef double sample_entropy(samples):
 
 # Implement the algorithm described in the paper:
 # Xu-Sheng Zhang et al. 1999. Detecting Ventricular Tachycardia and Fibrillation by Complexity Measure
-cdef double lz_complexity(samples):
+cdef double lz_complexity(np.ndarray[double, ndim=1] samples):
     cdef int cn = 0
 
     # find optimal threshold
@@ -445,8 +441,8 @@ cdef double lz_complexity(samples):
     return cn / bn
 
 
-cpdef preprocessing(samples, int sampling_rate, bint plotting=False):
-    samples = np.array(samples, dtype="float64")  # convert the signals from int to double
+cpdef np.ndarray[double, ndim=1] preprocessing(object src_samples, int sampling_rate, bint plotting=False):
+    cdef np.ndarray[double, ndim=1] samples = np.array(src_samples, dtype="float64")  # convert the signals from int to double
     cdef int n_samples = len(samples)
 
     if plotting:
