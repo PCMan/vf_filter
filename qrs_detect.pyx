a5f1863a891c43539f5a113daafdada7d89eb742
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2016-05-22 22:41:43 +0800

    Little fixes in the QRS detector related code.

--- qrs_detect.pyx
+++ qrs_detect.pyx
@@ -1,3 +1,9 @@
+# This is a quick and dirty python wrapper for the free QRS detector developed by EP Limited
+# PS Hamilton, WJ Tompkins. Quantitative investigation of QRS detection rules using the MIT/BIH arrhythmia database.
+# IEEE Trans. Biomed. Eng BME-33: 1158-1165 (1987).
+# http://www.eplimited.com/software.htm
+# The C code is put in osea20-gcc directory and it's licensed under GNU Library General Public License (LGPL).
+
 import pyximport; pyximport.install()  # use Cython
 import numpy as np
 import scipy as sp
@@ -10,16 +16,19 @@ import threading
 cdef extern int BeatDetectAndClassify(int ecgSample, int *beatType, int *beatMatch)
 cdef extern void ResetBDAC()
 
+# defined in osea20-gcc/bxbep.c
+cdef extern int amap(int a)
+
 # the underlying C library used for QRS detection is not thread-safe nor reentrant.
 # So, use a lock here to only allow calling from a thread at a time.
 _lock = threading.Lock()
 
 # This is not thread-safe due to the design flaw in osea20
-def qrs_detect(signals, int samplingRate, double gain):
+def qrs_detect(signals, int sampling_rate, double gain):
     beats = []
     # resample to 200Hz if needed
-    if samplingRate != 200:
-        signals = sp.signal.resample(signals, (len(signals) / samplingRate) * 200)
+    if sampling_rate != 200:
+        signals = sp.signal.resample(signals, (len(signals) / sampling_rate) * 200)
 
     _lock.acquire()
     ResetBDAC()  # reset the QRS detector
@@ -28,16 +37,17 @@ def qrs_detect(signals, int samplingRate, double gain):
     cdef int sample
     for sample_count, sample in enumerate(signals):  # send the samples to the beat detector one by one
         # Set baseline to 0 and resolution to 5 mV/lsb (200 units/mV)
-        # lTemp = ecg[0]-ADCZero ;
-        tmp = sample
-        tmp *= 200
-        tmp /= gain
-        sample = int(tmp)
+        # FIXME: remove ADC zero
+        # tmp = sample-ADCZero ;
+        sample = <int>(sample * 200 / gain)
+        beat_type = 0
+        beat_match = 0
         delay = BeatDetectAndClassify(sample, &beat_type, &beat_match)
         if delay:
             beat_time = sample_count - delay
-            time_str = str(datetime.timedelta(seconds=(beat_time / samplingRate)))
-            print(time_str, beat_type)
-            beats.append(beat_time)
+            # time_str = str(datetime.timedelta(seconds=(beat_time / sampling_rate)))
+            type_code = amap(beat_type)
+            # print(time_str, chr(type_code))
+            beats.append((beat_time, chr(type_code)))
     _lock.release()
     return beats
--- qrs_test.py
+++ qrs_test.py
@@ -9,14 +9,16 @@ import scipy.signal
 
 def main(args):
     record = vf_data.Record()
-    record.load("mitdb", "100", 0, "atr")
-    nsig = int(30 * record.sampling_rate)
-    signals = record.signals[:nsig]
+    record.load("vfdb", "422", 0, "atr")
+    n_sig = int(30 * record.sampling_rate)
+    begin_sig = 385788
+    signals = record.signals[begin_sig:begin_sig + n_sig]
     # resmaple to 200
     signals = sp.signal.resample(signals, (len(signals) / record.sampling_rate) * 200)
     beats = qrs_detect.qrs_detect(signals, 200, record.gain)
+    print(beats)
     plt.plot(signals)
-    plt.vlines(beats, 0, max(signals))
+    plt.vlines([b[0] for b in beats], 0, max(signals))
     plt.show()
     return 0
 
