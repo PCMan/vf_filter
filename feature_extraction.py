a73defba28ce51f3b1183189cd4054d1da669f32
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2016-05-28 17:35:39 +0800

    Using Pyro to implement distributed feature extraction though a cluster of servers (optional).

--- feature_extraction.py
+++ feature_extraction.py
@@ -51,23 +51,9 @@ def load_all_segments(args):
         idx += 1
 
 
-def main():
-    # parse command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--output", type=str, required=True)
-    parser.add_argument("-s", "--segment-duration", type=int, default=8)
-    parser.add_argument("-r", "--resample-rate", type=int, default=None)
-    parser.add_argument("-j", "--jobs", type=int, default=-1)
-    parser.add_argument("-v", "--verbose", action="store_true")
-    parser.add_argument("-u", "--update-features", type=str, nargs="+", choices=vf_features.feature_names, default=None)
-    parser.add_argument("-c", "--correction-file", type=str, help="Override the incorrect labels of the original dataset.")
-    args = parser.parse_args()
-
+def output_results(output_file, update_features, results):
     x_data_info = []
     x_data = []
-    # perform segmentation + feature extraction
-    parellel = Parallel(n_jobs=args.jobs, verbose=0, backend="multiprocessing", max_nbytes=2048)
-    results = parellel(delayed(extract_features)(idx, segment, args.resample_rate, args.update_features, args.verbose) for idx, segment in load_all_segments(args))
     # sort the results from multiple jobs according to the order they are emitted
     results.sort(key=lambda result: result[0])
     for idx, features, segment_info in results:
@@ -75,10 +61,10 @@ def main():
         x_data.append(features)
 
     # try to update existing feature file
-    if args.update_features:
+    if update_features:
         # load the old file
         try:
-            with open(args.output, "rb") as f:
+            with open(output_file, "rb") as f:
                 old_x_data = pickle.load(f)
                 old_x_data_info = pickle.load(f)
                 assert len(old_x_data) == len(x_data)
@@ -93,9 +79,96 @@ def main():
             print("Unable to load", args.output, sys.exc_info())
 
     # write to output files
-    with open(args.output, "wb") as f:
+    with open(output_file, "wb") as f:
         pickle.dump(x_data, f)
         pickle.dump(x_data_info, f)
+
+
+# distributed computing server
+class Server:
+    def __init__(self, daemon, args):
+        self.daemon = daemon
+        self.segment_iter = iter(load_all_segments(args))
+        self.n_all_segments = 0
+        self.finish_read = False
+        self.results = []
+
+    # called by slaves to get next job to compute
+    def next_segment(self):
+        if not self.finish_read:
+            try:
+                idx, segment = next(self.segment_iter)
+                self.n_all_segments += 1
+                return idx, segment 
+            except StopIteration:
+                self.finish_read = True  # all data are fetched
+        return -1, None  # no more data to compute
+
+    def add_results(self, results):  # receive results from slaves
+        self.results.extend(results)
+        print("got results", self.finish_read, len(self.results), self.n_all_segments)
+        if self.finish_read and len(self.results) == self.n_all_segments:
+            # all computation are done, stop the server
+            self.daemon.shutdown()
+            del self.daemon  # release the reference to daemon object
+
+
+# this is a generator function
+def load_all_segments_from_server(master_proxy, args):
+    while True:
+        # get next segment to compute from the server
+        idx, segment = master_proxy.next_segment()
+        if idx != -1 and segment:
+            yield idx, segment
+        else:  # no more task to do
+            break
+
+
+def parallel_extract_features(args, data_generator):
+    parellel = Parallel(n_jobs=args.jobs, verbose=0, backend="multiprocessing", max_nbytes=2048)
+    results = parellel(delayed(extract_features)(idx, segment, args.resample_rate, args.update_features, args.verbose) for idx, segment in data_generator)
+    return results
+
+
+def main():
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", type=str)
+    parser.add_argument("-s", "--segment-duration", type=int, default=8)
+    parser.add_argument("-r", "--resample-rate", type=int, default=None)
+    parser.add_argument("-j", "--jobs", type=int, default=-1)
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-u", "--update-features", type=str, nargs="+", choices=vf_features.feature_names, default=None)
+    parser.add_argument("-c", "--correction-file", type=str, help="Override the incorrect labels of the original dataset.")
+
+    # for distributed computing using Pyro4
+    parser.add_argument("-l", "--listen-port", type=int, help="Launch a distributed computing server to collect the computed features on the port number.")
+    parser.add_argument("-m", "--master-uri", type=str, help="Launch a distributed computing slave pulling jobs from the server and send the results back.")
+    args = parser.parse_args()
+
+    # perform feature extraction task in parellel (optionally using master/slave distributed computing)
+    if args.master_uri:  # if we're a slave, connect to the master to fetch data to compute
+        import Pyro4
+        Pyro4.config.SERIALIZERS_ACCEPTED = {"pickle"}
+        Pyro4.config.SERIALIZER = "pickle"
+        master_proxy = Pyro4.Proxy(args.master_uri)  # connect to the remote server object
+        results = parallel_extract_features(args, load_all_segments_from_server(master_proxy, args))
+        master_proxy.add_results(results)  # send the results to the master
+    elif args.listen_port:  # if we're launching a master, create the server object
+        import Pyro4
+        Pyro4.config.SERIALIZERS_ACCEPTED = {"pickle"}
+        Pyro4.config.SERIALIZER = "pickle"
+        pyro_daemon = Pyro4.Daemon(port=args.listen_port)
+        server = Server(pyro_daemon, args)
+        uri = pyro_daemon.register(server)
+        print("Launch server at", uri)
+        pyro_daemon.requestLoop()  # blocked until all data are processed
+        results = server.results
+    else:  # do not use distributed computing, parellel on this machine with multi-core only
+        results = parallel_extract_features(args, load_all_segments(args))
+
+    if not args.master_uri and args.output:  # if we're not a computing slave, save the results to disks
+        output_results(args.output, args.update_features, results)
 
 
 if __name__ == "__main__":
